7d43ef1aa735f77b5efef4537b80a2371043a035
Roop <rooppadala@Roops-MacBook-Air.local>
2021-06-01 16:46:49 -0500

    adding tests

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [data, setData] = useState(null);
+
+ useEffect(() => {
+    fetch("/getbag")
+      .then((res) => res.json())
+      .then((data) => setData(data));
+  }, []);
+  
   return (
     <div className="App">
-      testing
+      <header className="app-header">
+        <p>{!data ? "Loading..." : data}</p>
+      </header>
     </div>
   );
 }
+
 
 export default App;
