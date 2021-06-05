a454cf7db938ccc3cb24fdca4c549e9e68c01b8a
Roop <rooppadala@Roops-MacBook-Air.local>
2021-06-05 09:42:57 -0500

    Header styles

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,13 @@
 
+import "./Header.css"
 type headerProps  = {
     itemsCount: string
 }
 
 export const Header = ({itemsCount}:headerProps) => {
-    return (<div className="bag-count">
-        {itemsCount}
-    </div>)
+    return (<div className="header-section">
+    <h4>Gap Shopping bag</h4>
+    <div className="bag-count">
+        {itemsCount} Items
+    </div></div>)
 }
--- src/components/__tests__/Header.test.tsx
+++ src/components/__tests__/Header.test.tsx
@@ -2,12 +2,18 @@ import { shallow } from '../../enzyme';
 
 import { Header } from '../Header';
 
+const itemsCount = "3";
 
 describe('Header tests', () => {
+    it('Should show header', () => {
+        const wrapper = shallow(<Header itemsCount={itemsCount} />);
+        expect(wrapper.find('.header-section h4').text()).toEqual("Gap Shopping bag");
+    });
+
     it('Should show bag items count', () => {
-        const itemsCount = "3";
+        
         const wrapper = shallow(<Header itemsCount={itemsCount} />);
         expect(wrapper.find('.bag-count')).toBeDefined();
-        expect(wrapper.find('.bag-count').text()).toEqual("3");
+        expect(wrapper.find('.bag-count').text()).toEqual("3 Items");
     });
 });
